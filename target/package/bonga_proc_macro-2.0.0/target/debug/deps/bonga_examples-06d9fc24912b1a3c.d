702a11d9cc9c6ee1bf5ccc6606f3531b824a0860
Shivnath Kisku <nomono751@gmail.com>
2023-07-07 13:19:08 +0530

    Initial Commit

---
+++ target/package/bonga_proc_macro-2.0.0/target/debug/deps/bonga_examples-06d9fc24912b1a3c.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Shivnath Kisku/Music/bonga/target/debug/deps/bonga_examples-06d9fc24912b1a3c.rmeta: examples/src/main.rs
+
+/mnt/c/Users/Shivnath Kisku/Music/bonga/target/debug/deps/bonga_examples-06d9fc24912b1a3c.d: examples/src/main.rs
+
+examples/src/main.rs:
---
+++ target/package/bonga_proc_macro-2.0.0/target/debug/examples/src-9783e0bec2e8ba9c.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Shivnath Kisku/Music/bonga/target/debug/examples/src-9783e0bec2e8ba9c.rmeta: examples/src/main.rs
+
+/mnt/c/Users/Shivnath Kisku/Music/bonga/target/debug/examples/src-9783e0bec2e8ba9c.d: examples/src/main.rs
+
+examples/src/main.rs:
